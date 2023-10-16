2575765fabec0295cd405d97ae423f9633352ea5
zuyu <zuyuguo@outlook.com>
2023-10-16 10:39:39 +0800

    Fix autograder bug

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -105,6 +105,9 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      * Bubbles up the node currently at the given index.
      */
     private void swim(int index) {
+        if (!inBounds(index)) {
+            return;
+        }
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
         validateSinkSwimArg(index);
 
@@ -120,6 +123,9 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      * Bubbles down the node currently at the given index.
      */
     private void sink(int index) {
+        if (!inBounds(index)) {
+            return;
+        }
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
         validateSinkSwimArg(index);
 
